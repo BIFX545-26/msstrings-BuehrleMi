cefd8618e90d62b8c7a532707d1e4bdfa985f432
BuehrleMi <mab31@hood.edu>
2026-02-17 18:59:32 -0500

    021726

---
+++ R/data.R
@@ -0,0 +1,10 @@
+# data.R
+# Documentation for our data
+
+#' MSfrags
+#' Sample peptide fragments in Spectronaut format
+#' 
+#' @format A character vector containing 4 sample peptide fragments with protein modifications in Spectronaut format
+#' @source These fragments were generated from P51681 (for details, see...)
+#' 
+"MSfrags"
---
+++ tests/testthat/test-parse_mods.R
@@ -0,0 +1,5 @@
+#test the parse_mods function
+
+test_that("parse_mods works", {
+  expect_equal(2 * 2, 4)
+})
